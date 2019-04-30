a31eea31113950ce698cc3897708b4aefd0c7cf5
ramonsq <chimpin3000@hotmail.es>
2019-05-01 00:34:49 +0200

    -changed some details from the server (not commented yet) -put everything outside the basic level folder

--- server.py
+++ server.py
@@ -4,6 +4,15 @@ import requests
 import termcolor
 import sys
 
+PORT = 8000
+
+
+def error_page():
+    # Show 404 page
+    file = open("Error.html", "r")
+    return file.read()
+# ----------------------------------------------
+
 
 class MyServer(BaseHTTPRequestHandler):
     def do_HEAD(self):
@@ -35,21 +44,27 @@ class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         termcolor.cprint(self.requestline + "\n", 'green')
         action = self.path.split("?")[0]
-
+    # --------------------------------------------------
         if action == "/":
             file = open("index.html","r")
             contents = file.read()
+    # -------------------------------------------------------------------------------
         elif action == "/listSpecies":
-            if self.get_input_value():
-                list_of_species = self.list_species(self.get_input_value())
-            else:
-                list_of_species = self.list_species("all")
-            file = open("listSpecies.html", "r")
-            contents = file.read()
-            contents += "<h1>Here is a list of species:</h1><ul>"
-            for item in list_of_species:
-                contents += "<li>" + item + "</li>"
-            contents += "</ul></body></html>"
+            input_value = self.get_input_value()
+            try:
+                if input_value:
+                    list_of_species = self.list_species(int(input_value))
+                else:
+                    list_of_species = self.list_species("all")
+                file = open("listSpecies.html", "r")
+                contents = file.read()
+                contents += "<h1>Here is the list of species:</h1><ul>"
+                for item in list_of_species:
+                    contents += "<li>" + item + "</li>"
+                contents += "</ul></body></html>"
+            except ValueError:
+                contents = error_page()
+    # -------------------------------------------------------------------------------
         elif action == "/karyotype":
             file = open("karyotype.html", "r")
             contents = file.read()
@@ -57,23 +72,32 @@ class MyServer(BaseHTTPRequestHandler):
                 kt = self.karyotype(self.get_input_value())
                 contents += "Karyotype of " + self.get_input_value().upper() + ":<ul>"
                 if len(kt) == 0:
-                    contents += "<p><strong>None</strong></p>"
+                    contents += "<p><strong>It has None Karyotype</strong></p>"
+                #elif str(kt[0]).split(" ")[0] != '200':
+                    # Show 404 page
+                 #   contents = error_page()
                 else:
                     for item in kt:
                         contents += "<li>" + item + "</li>"
             contents += "</body></html>"
+    # ---------------------------------------------------------------------------------
         elif action == "/chromosomeLength":
             file = open("chromosomeLength.html", "r")
             contents = file.read()
             if self.get_input_value():
                 specie, chromo = self.get_input_value()
                 contents += "The Chromosome " + chromo.upper() + " lenght of " + specie.upper() + ": "
-                contents += "<strong>" + str(self.chromosomeLength(specie, chromo.upper())) + "</strong>"
+                chrom_len = self.chromosomeLength(specie, chromo.upper())
+                if isinstance(chrom_len,int):
+                    contents += "<strong>" + str(chrom_len) + "</strong>"
+                else:
+                    contents = error_page()
+
             contents += "</body></html>"
+    # ---------------------------------------------------------------------------------
         else:
             # Show 404 page
-            file = open("Error.html", "r")
-            contents = file.read()
+            contents = error_page()
 
         self.send_response(200)  # -- Status line: OK!
         self.send_header('Content-Type', 'text/html')
@@ -81,7 +105,8 @@ class MyServer(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(str.encode(contents))
 
-    def list_species(self, limit=200):
+# ---------------------------------------------------------------------------------
+    def list_species(self, limit):
         server = "https://rest.ensembl.org"
         ext = "/info/species?"
         r = requests.get(server + ext, headers={"Content-Type": "application/json"})
@@ -91,7 +116,7 @@ class MyServer(BaseHTTPRequestHandler):
         decoded = r.json()
         results = decoded.get('species')
         sp_list = []
-        if limit == "all":
+        if limit == 'all':
             n = len(results)
         else:
             n = int(limit)
@@ -106,41 +131,44 @@ class MyServer(BaseHTTPRequestHandler):
                     break
         return sp_list
 
+# ---------------------------------------------------------------------------------
     def karyotype(self, specie):
         server = "http://rest.ensembl.org"
         ext = "/info/assembly/" + specie
         r = requests.get(server + ext, headers={"Content-Type": "application/json"})
         if not r.ok:
-            r.raise_for_status()
-            sys.exit()
+            try:
+                r.raise_for_status()
+                return r.status_code
+            except Exception as e:
+                return e.args
+
         decoded = r.json()
         kt_list = decoded['karyotype']
         return kt_list
 
+# ---------------------------------------------------------------------------------
     def chromosomeLength(self, specie, chromo):
         server = "http://rest.ensembl.org"
         ext = "/info/assembly/"+specie
         r = requests.get(server + ext, headers={"Content-Type": "application/json"})
         if not r.ok:
-            r.raise_for_status()
-            sys.exit()
+            try:
+                r.raise_for_status()
+                return r.status_code
+            except Exception as e:
+                return e.args
+
         decoded = r.json()
         results = decoded.get('top_level_region')
         if results:
             for item in results:
                 if item['name'] == chromo:
                     return item['length']
-            return "Not found!"
+            return "None"
 
+# ---------------------------------------------------------------------------------
 
-HOST_NAME = 'localhost'
-PORT = 8000
-
-routes = {
-    '/': 'Hello World!',
-    '/bye': 'Goodbye World!',
-    '/favicon.ico': ''
-}
 
 if __name__ == '__main__':
     # ------------------------
@@ -153,7 +181,7 @@ if __name__ == '__main__':
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
         print("Serving at PORT", PORT)
         # -- Main loop: Attend the client. Whenever there is a new
-        # -- client, the handler is called
+        # -- clint, the handler is called
         try:
             httpd.serve_forever()
         except KeyboardInterrupt:
@@ -161,5 +189,4 @@ if __name__ == '__main__':
             print("Stopped by the user")
             httpd.server_close()
 
-    print("")
     print("Server Stopped")

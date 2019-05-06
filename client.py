b17e9f9e9aa7ccaf1306ee1f26b4fd8c0257afbd
ramonsq <chimpin3000@hotmail.es>
2019-05-06 13:53:06 +0200

    with advance to try at lab

---
+++ client.py
@@ -0,0 +1,23 @@
+import requests
+import sys
+
+# SERVER IP, PORT
+SERVER = "http://localhost:8000/"
+
+endpoint = input(SERVER)
+
+if endpoint.find("=") > 0:
+    endpoint += "&json=1"
+else:
+    endpoint += "?json=1"
+
+r = requests.get(SERVER+endpoint, headers={"Content-Type": "application/json"})
+ 
+if not r.ok:
+  r.raise_for_status()
+  sys.exit()
+ 
+decoded = r.json()
+print(repr(decoded))
+
+input()

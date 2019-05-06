be2493eeb71f2a29e4efe80afa395b260750a6e0
ramonsq <chimpin3000@hotmail.es>
2019-05-06 23:39:20 +0200

    client done with some changes

--- client.py
+++ client.py
@@ -3,21 +3,23 @@ import sys
 
 # SERVER IP, PORT
 SERVER = "http://localhost:8000/"
-
+print("-----------------------------------------------------------")
+print("Enter the endpoint below, press ENTER and wait for 'Result'")
+print("-----------------------------------------------------------")
 endpoint = input(SERVER)
 
-if endpoint.find("=") > 0:
-    endpoint += "&json=1"
+if endpoint.endswith("json=1"):
+    r = requests.get(SERVER+endpoint, headers={ "Content-Type" : "application/json"})
+    if not r.ok:
+        r.raise_for_status()
+        sys.exit()
+    try:
+        decoded = r.json()
+        print("\nResult:\n"+repr(decoded))
+    except Exception as e:
+        print("\nError: "+str(e.args))
 else:
-    endpoint += "?json=1"
+    print("\nError: 'json=1' parameter is missing")
 
-r = requests.get(SERVER+endpoint, headers={"Content-Type": "application/json"})
- 
-if not r.ok:
-    r.raise_for_status()
-    sys.exit()
- 
-decoded = r.json()
-print(repr(decoded))
 
 input()

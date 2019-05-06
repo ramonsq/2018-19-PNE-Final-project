efc87c31b1c786965cfd66dfd79de8e82d37fa04
ramonsq <chimpin3000@hotmail.es>
2019-05-06 23:45:27 +0200

    PRACTICE DONE

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler
 import socketserver
 import requests
 import termcolor
+import json
 
 PORT = 8000
 
@@ -14,10 +15,21 @@ def error_page():
 
 
 class MyServer(BaseHTTPRequestHandler):
+    JSON = False
+
+    def verify_client(self):
+        if self.path.split('?')[1].endswith('&json=1'):
+            self.JSON = True
+            return self.path.split('?')[1].replace('&json=1', '')
+        elif self.path.split('?')[1].endswith('json=1'):
+            self.JSON = True
+            return self.path.split('?')[1].replace('json=1', '')
+        else:
+            return self.path.split('?')[1]
 
     def get_list_input(self):
         if self.path.find('?') > 0:
-            user_input = self.path.split("?")[1]
+            user_input = self.verify_client()
             if self.path.find('='):
                 if self.path.find('&') > 0:
                     input_a = user_input.split("&")[0]
@@ -36,9 +48,12 @@ class MyServer(BaseHTTPRequestHandler):
 
     def get_input_value(self):
         if self.path.find('?') > 0:
-            user_input = self.path.split("?")[1]
-            if self.path.find('='):
-                if self.path.find('&') > 0:
+            # user_input = self.path.split("?")[1]
+            user_input = self.verify_client()
+            if len(user_input) == 0:
+                return False
+            if user_input.find('='):
+                if user_input.find('&') > 0:
                     input_a = user_input.split("&")[0]
                     input_b = user_input.split("&")[1]
                     value_a = input_a.split("=")[1]
@@ -72,113 +87,148 @@ class MyServer(BaseHTTPRequestHandler):
                     list_of_species = self.list_species(int(input_value))
                 else:
                     list_of_species = self.list_species("all")
-                file = open("listSpecies.html", "r")
-                contents = file.read()
-                contents += "<h1>Here is the list of species:</h1><ul>"
-                for item in list_of_species:
-                    contents += "<li>" + item + "</li>"
-                contents += "</ul></body></html>"
+                if self.JSON:
+                    contents = json.dumps(list_of_species)
+                else:
+                    file = open("listSpecies.html", "r")
+                    contents = file.read()
+                    contents += "<h1>Here is the list of species:</h1><ul>"
+                    for item in list_of_species:
+                        contents += "<li>" + item + "</li>"
+                    contents += "</ul></body></html>"
             except ValueError:
-                contents = error_page()
+                if self.JSON:
+                    print("Error: There is something wrong with your request!")
+                else:
+                    contents = error_page()
     # -------------------------------------------------------------------------------
         elif action == "/karyotype":
             file = open("karyotype.html", "r")
             contents = file.read()
-            if self.get_input_value():
-                kt = self.karyotype(self.get_input_value())
-                contents += "Karyotype of " + self.get_input_value().upper() + ":<ul>"
-                if not isinstance(kt, list):
-                    # Show 404 page
-                    print(kt)
-                    contents = error_page()
-                elif len(kt) == 0:
-                    contents += "<p><strong>It has None Karyotype</strong></p>"
+            input_value = self.get_input_value()
+            if input_value:
+                kt = self.karyotype(input_value)
+                if self.JSON:
+                    contents = json.dumps(kt)
                 else:
-                    for item in kt:
-                        contents += "<li>" + item + "</li>"
-            contents += "</body></html>"
+                    contents += "Karyotype of " + input_value.upper() + ":<ul>"
+                    if not isinstance(kt, list):
+                        # Show 404 page
+                        print(kt)
+                        contents = error_page()
+                    elif len(kt) == 0:
+                        contents += "<p><strong>It has None Karyotype</strong></p>"
+                    else:
+                        for item in kt:
+                            contents += "<li>" + item + "</li>"
+            if not self.JSON:
+                contents += "</body></html>"
     # ---------------------------------------------------------------------------------
         elif action == "/chromosomeLength":
             file = open("chromosomeLength.html", "r")
             contents = file.read()
-            if self.get_input_value():
-                specie, chromo = self.get_input_value()
-                contents += "The Chromosome " + chromo.upper() + " lenght of " + specie.upper() + ": "
-                chrom_len = self.chromosomeLength(specie, chromo.upper())
-                if isinstance(chrom_len, int):
-                    contents += "<strong>" + str(chrom_len) + "</strong>"
+            input_value = self.get_input_value()
+            if input_value:
+                specie, chromo = input_value
+                if self.JSON:
+                    contents = json.dumps(self.chromosomeLength(specie, chromo.upper()))
                 else:
-                    contents = error_page()
-            contents += "</body></html>"
+                    contents += "The Chromosome " + chromo.upper() + " lenght of " + specie.upper() + ": "
+                    chrom_len = self.chromosomeLength(specie, chromo.upper())
+                    if isinstance(chrom_len, int):
+                        contents += "<strong>" + str(chrom_len) + "</strong>"
+                    else:
+                        contents = error_page()
+
     # ---------------------------------------------------------------------------------> M E D I U M
         elif action == "/geneSeq":
             file = open("geneSeq.html", "r")
             contents = file.read()
-            if self.get_input_value():
-                gene = self.get_input_value()
-                contents += "Sequence of human gene " + gene.upper() + ": "
-                seq = self.geneSeq(gene.upper())
-                if isinstance(seq, str):
-                    contents += "<br><div style=\"overflow-wrap: break-word;\">"
-                    contents += "<strong>" + str(seq) + "</strong></div>"
+            input_value = self.get_input_value()
+            if input_value:
+                gene = input_value
+                if self.JSON:
+                    contents = json.dumps(self.geneSeq(gene.upper()))
                 else:
-                    contents = error_page()
-            contents += "</body></html>"
+                    contents += "Sequence of human gene " + gene.upper() + ": "
+                    seq = self.geneSeq(gene.upper())
+                    if isinstance(seq, str):
+                        contents += "<br><div style=\"overflow-wrap: break-word;\">"
+                        contents += "<strong>" + str(seq) + "</strong></div>"
+                    else:
+                        contents = error_page()
+            if not self.JSON:
+                contents += "</body></html>"
     # --------------------------------------------------------------------------------
         elif action == "/geneInfo":
             file = open("geneInfo.html", "r")
             contents = file.read()
-            if self.get_input_value():
-                gene = self.get_input_value()
-                contents += "<h2>Information about human gene " + gene.upper() + ":</h2>"
-                gene_info = self.geneInfo(gene.upper())
-                if isinstance(gene_info, dict):
-                    contents += "<p> ID: " + gene_info['id'] + "</p>"
-                    contents += "<p> Start: " + gene_info['start'] + "</p>"
-                    contents += "<p> End: " + gene_info['end'] + "</p>"
-                    contents += "<p> Lenght: " + gene_info['lenght'] + "</p>"
-                    contents += "<p> Chromosome: " + gene_info['chromo'] + "</p>"
+            input_value = self.get_input_value()
+            if input_value:
+                gene = input_value
+                if self.JSON:
+                    contents = json.dumps(self.geneInfo(gene.upper()))
                 else:
-                    contents = error_page()
-            contents += "</body></html>"
+                    contents += "<h2>Information about human gene " + gene.upper() + ":</h2>"
+                    gene_info = self.geneInfo(gene.upper())
+                    if isinstance(gene_info, dict):
+                        contents += "<p> ID: " + gene_info['id'] + "</p>"
+                        contents += "<p> Start: " + gene_info['start'] + "</p>"
+                        contents += "<p> End: " + gene_info['end'] + "</p>"
+                        contents += "<p> Lenght: " + gene_info['lenght'] + "</p>"
+                        contents += "<p> Chromosome: " + gene_info['chromo'] + "</p>"
+                    else:
+                        contents = error_page()
+            if not self.JSON:
+                contents += "</body></html>"
     # --------------------------------------------------------------------------------
         elif action == "/geneCalc":
             file = open("geneCalc.html", "r")
             contents = file.read()
-            if self.get_input_value():
-                gene = self.get_input_value()
-                contents += "<h2>Information about human gene " + gene.upper() + " bases:</h2>"
-                bases = self.geneCalc(gene.upper())
-                if isinstance(bases, dict):
-                    contents += "<h4>Lenghts:</h4><ul>"
-                    contents += "<li> A: " + bases['ba'] + "</li>"
-                    contents += "<li> C: " + bases['bc'] + "</li>"
-                    contents += "<li> G: " + bases['bg'] + "</li>"
-                    contents += "<li> T: " + bases['bt'] + "</li></ul>"
-                    contents += "<h4>Percentages:</h4><ul>"
-                    contents += "<li> A: " + bases['pa'] + "</li>"
-                    contents += "<li> C: " + bases['pc'] + "</li>"
-                    contents += "<li> G: " + bases['pg'] + "</li>"
-                    contents += "<li> T: " + bases['pt'] + "</li></ul>"
+            input_value = self.get_input_value()
+            if input_value:
+                gene = input_value
+                if self.JSON:
+                    contents = json.dumps(self.geneCalc(gene.upper()))
                 else:
-                    contents = error_page()
-            contents += "</body></html>"
+                    contents += "<h2>Information about human gene " + gene.upper() + " bases:</h2>"
+                    bases = self.geneCalc(gene.upper())
+                    if isinstance(bases, dict):
+                        contents += "<h4>Lenghts:</h4><ul>"
+                        contents += "<li> A: " + bases['Base A'] + "</li>"
+                        contents += "<li> C: " + bases['Base C'] + "</li>"
+                        contents += "<li> G: " + bases['Base G'] + "</li>"
+                        contents += "<li> T: " + bases['Base T'] + "</li></ul>"
+                        contents += "<h4>Percentages:</h4><ul>"
+                        contents += "<li> A: " + bases['Percentage A'] + "</li>"
+                        contents += "<li> C: " + bases['Percentage C'] + "</li>"
+                        contents += "<li> G: " + bases['Percentage G'] + "</li>"
+                        contents += "<li> T: " + bases['Percentage T'] + "</li></ul>"
+                    else:
+                        contents = error_page()
+            if not self.JSON:
+                contents += "</body></html>"
     # --------------------------------------------------------------------------------
         elif action == "/geneList":
             file = open("geneList.html", "r")
             contents = file.read()
-            if self.get_input_value():
+            input_value = self.get_input_value()
+            if input_value:
                 chromo, start, end = self.get_list_input()
-                contents += "<h2>Genes located in the chromosome " + chromo.upper() + " </h2>"
-                names = self.geneList(chromo, start, end)
-                if isinstance(names, list):
-                    contents += "<ul>"
-                    for item in names:
-                        contents += "<li>" + item + "</li>"
-                    contents += "</ul>"
+                if self.JSON:
+                    contents = json.dumps(self.geneList(chromo, start, end))
                 else:
-                    contents = error_page()
-            contents += "</body></html>"
+                    contents += "<h2>Genes located in the chromosome " + chromo.upper() + " </h2>"
+                    names = self.geneList(chromo, start, end)
+                    if isinstance(names, list):
+                        contents += "<ul>"
+                        for item in names:
+                            contents += "<li>" + item + "</li>"
+                        contents += "</ul>"
+                    else:
+                        contents = error_page()
+            if not self.JSON:
+                contents += "</body></html>"
 
     # --------------------------------------------------------------------------------
         else:
@@ -186,10 +236,13 @@ class MyServer(BaseHTTPRequestHandler):
             contents = error_page()
 
         self.send_response(200)  # -- Status line: OK!
-        self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', len(str.encode(contents)))
-        self.end_headers()
-        self.wfile.write(str.encode(contents))
+        if self.JSON:
+            self.send_header('Content-Type', 'application/json')
+        else:
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
+            self.end_headers()
+            self.wfile.write(str.encode(contents))
 
 # ---------------------------------------------------------------------------------
     def list_species(self, limit):
@@ -315,14 +368,14 @@ class MyServer(BaseHTTPRequestHandler):
             if not r.ok:
                 r.raise_for_status()
             data = {
-                "ba": str(r.text.count('A')),
-                "bc": str(r.text.count('C')),
-                "bg": str(r.text.count('G')),
-                "bt": str(r.text.count('T')),
-                "pa": str('{:0.2f}'.format(100 * int(r.text.count('A')) / len(r.text))) + "%",
-                "pc": str('{:0.2f}'.format(100 * int(r.text.count('C')) / len(r.text))) + "%",
-                "pg": str('{:0.2f}'.format(100 * int(r.text.count('G')) / len(r.text))) + "%",
-                "pt": str('{:0.2f}'.format(100 * int(r.text.count('T')) / len(r.text))) + "%"
+                "Base A": str(r.text.count('A')),
+                "Base C": str(r.text.count('C')),
+                "Base G": str(r.text.count('G')),
+                "Base T": str(r.text.count('T')),
+                "Percentage A": str('{:0.2f}'.format(100 * int(r.text.count('A')) / len(r.text))) + "%",
+                "Percentage C": str('{:0.2f}'.format(100 * int(r.text.count('C')) / len(r.text))) + "%",
+                "Percentage G": str('{:0.2f}'.format(100 * int(r.text.count('G')) / len(r.text))) + "%",
+                "Percentage T": str('{:0.2f}'.format(100 * int(r.text.count('T')) / len(r.text))) + "%"
             }
             return data
         except Exception as e:
@@ -332,7 +385,8 @@ class MyServer(BaseHTTPRequestHandler):
     def geneList(self, chromo, start, end):
         try:
             server = "http://rest.ensembl.org"
-            ext = "/overlap/region/human/" + str(chromo) + ":" + str(start) + "-" + str(end) + "?content-type=application/json;feature=gene"
+            ext = "/overlap/region/human/" + str(chromo) + ":" + str(start) + "-" + str(end)
+            ext += "?content-type=application/json;feature=gene"
             r = requests.get(server + ext, headers={"Content-Type": "application/json"})
             if not r.ok:
                 r.raise_for_status()
